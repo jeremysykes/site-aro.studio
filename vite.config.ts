7b45dbc2a8946c989afb720477a0e7412ea0e1dd
Jeremy Sykes <jeremy.sykes.office@gmail.com>
2026-01-27 14:39:16 -0500

    Add Testing: Vitest, jsdom, RTL, setup, i18n in tests, mocks, App smoke test

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    include: ['src/**/*.test.{ts,tsx}'],
+    exclude: ['node_modules', 'dist', '.storybook', '**/*.stories.{ts,tsx}'],
+  },
 })
